8ac8cacf9294aa397089e1d562e6a65167b49f49
Tyler Lange <neosmedia@gmail.com>
2017-09-14 16:36:47 -0700

    navbar-side working. FA icons in header.

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,18 @@
+
 $(document).ready(function(){
+
+// Open navbarSide when button is clicked
+          $('#navbarSideButton').on('click', function() {
+            $('#navbarSide').addClass('reveal');
+            $('.navbar-overlay').show();
+          });
+
+          // Close navbarSide when the outside of menu is clicked
+          $('.navbar-overlay').on('click', function(){
+            $('#navbarSide').removeClass('reveal');
+            $('.navbar-overlay').hide();
+          });
+
 /* ==========================================================================
 	Scroll
 	========================================================================== */
